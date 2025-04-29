aae8f337312433217e656a9e726a6305e479134f
Jab <jabbarpanggabean@gmail.com>
2025-04-29 22:24:40 +0700

    first commit

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   <div class="container mx-auto py-10 px-4">
     <h1 class="text-2xl font-bold mb-6">Edit Tugas</h1>
-
+    @include('components.alerts')
     <form action="{{ route('tasks.update', $task) }}" method="POST">
       @method('PUT')
       @include('tasks._form')
